3c7a7d374aaac22738e304bb4fee80caf2a58fd8
明石凜太 <jeffrey.bezos@icloud.com>
2023-07-09 02:56:29 +0900

    action1

--- Backend/packages/create_dao.go
+++ Backend/packages/create_dao.go
@@ -2,14 +2,9 @@ package packages
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	_ "github.com/go-sql-driver/mysql"
 )
-type Message struct {
-    Text string `json:"text"`
-}
-
 
 type Student struct {
 	StudentID string `json:"student_id"`
@@ -20,12 +15,12 @@ type Student struct {
 	Hobby     string `json:"hobby"`
 }
 
-func Create(s Student) (Student, error) {
+func Create(s Student) interface{} {
 	// DB接続
 	db, err := sql.Open("mysql", "LAA1417803:LAA1417803@tcp(mysql215.phy.lolipop.lan)/LAA1417803-matching?charset=utf8")
 	if err != nil {
 		log.Fatal(err)
-		return Student{}, fmt.Errorf("database connection failed: %v", err)
+		return "database connection failed: " + err.Error()
 	}
 	defer db.Close()
 
@@ -34,33 +29,26 @@ func Create(s Student) (Student, error) {
 	err = db.QueryRow("SELECT COUNT(*) FROM students WHERE student_id = ?", s.StudentID).Scan(&exists)
 	if err != nil {
 		log.Fatal(err)
-		return Student{}, fmt.Errorf("failed to check if student ID exists: %v", err)
+		return "failed to check if student ID exists: " + err.Error()
 	}
 
 	if exists > 0 {
-		return Student{}, fmt.Errorf("the student ID already exists")
+		return "the student ID already exists"
 	}
 
 	// インサート処理
 	stmt, err := db.Prepare("INSERT INTO students(student_id, name, age, gender, password, hobby) VALUES(?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
-		return Student{}, fmt.Errorf("failed to prepare insert statement: %v", err)
+		return "failed to prepare insert statement: " + err.Error()
 	}
 
 	_, err = stmt.Exec(s.StudentID, s.Name, s.Age, s.Gender, s.Password, s.Hobby)
 	if err != nil {
 		log.Fatal(err)
-		return Student{}, fmt.Errorf("failed to execute insert statement: %v", err)
+		return "failed to execute insert statement: " + err.Error()
 	}
 
 	// インサートした情報を返す
-	return Student{
-		StudentID: s.StudentID,
-		Name:      s.Name,
-		Age:       s.Age,
-		Gender:    s.Gender,
-		Password:  s.Password,
-		Hobby:     s.Hobby,
-	}, nil
+	return s
 }
