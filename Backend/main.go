40f7ed483d3093a5456651f9070d59ec9b2e7d62
明石凜太 <jeffrey.bezos@icloud.com>
2023-07-10 12:32:42 +0900

    s

--- Backend/main.go
+++ Backend/main.go
@@ -1,30 +1,72 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
-	"github.com/matching/Backend/matching"
+	"log"
+	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	StudentId string `json:"student_id"`
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	Gender    string `json:"gender"`
+	Password  string `json:"password"`
+	HobbyId string `json:"hobby_id"`
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-			return
-		}
+	r := mux.NewRouter()
+	r.HandleFunc("/create", CreateUser).Methods("POST", "OPTIONS")
+	http.ListenAndServe(":8080", r)
+}
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		var s matching.Student // <- matching.Student型を使用
-		err := json.NewDecoder(r.Body).Decode(&s)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
+    // Stop here if its Preflighted OPTIONS request
+    if r.Method == "OPTIONS" {
+        return
+    }
 
-		message := matching.Create(s) // <- matching.Create関数を使用
+    var user User
+    err := json.NewDecoder(r.Body).Decode(&user)
+    if err != nil {
+        w.Header().Set("Content-Type", "application/json")
+        http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusBadRequest)
+        return
+    }
 
+    db, err := sql.Open("mysql", "LAA1417803:LAA1417803@tcp(mysql215.phy.lolipop.lan)/LAA1417803-matching?charset=utf8")
+
+    if err != nil {
+		log.Println(err.Error())
+        w.Header().Set("Content-Type", "application/json")
+        http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusInternalServerError)
+        return
+    }
+    defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO users(student_id, name, age, gender, password, hobby_id) VALUES(?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		log.Println(err.Error())
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(message)
-	})
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusInternalServerError)
+		return
+	}
+	_, err = stmt.Exec(user.StudentId, user.Name, user.Age, user.Gender, user.Password, user.HobbyId)
+	if err != nil {
+		log.Println(err.Error())
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, `{"error": "`+err.Error()+`"}`, http.StatusInternalServerError)
+		return
+	}
+	
 
-	http.ListenAndServe(":8000", nil)
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(map[string]string{"message": "User created successfully"})
 }
